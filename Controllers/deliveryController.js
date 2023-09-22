22bd5816bcf4ad402d38f56f8069aa75ad3cf978
BerardiAle <berardi.alejandro@gmail.com>
2023-09-22 06:55:02 -0300

    first commit

---
+++ Controllers/deliveryController.js
@@ -0,0 +1,10 @@
+const envio = document.getElementById('envio');
+envio.addEventListener('click', showMesagge);
+
+
+const lorem = document.getElementById('lorem');
+
+
+function showMesagge(){
+    lorem.innerHTML = "Script on load";
+}
